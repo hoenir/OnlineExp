fdf77608330352de9d1cb00591bb8da7933f5afd
Hoenir <sander.vandecruys@gmail.com>
2016-01-19 01:20:42 +0100

    before version with belief triangle

---
+++ sfmCreaFlex/js/timer.js
@@ -0,0 +1,25 @@
+//self-adjusting timer abstraction
+		function doTimer(length, resolution, oninstance, oncomplete)
+		{
+			var steps = (length / 100) * (resolution / 10),
+				speed = length / steps,
+				count = 0,
+				start = new Date().getTime();
+
+			function instance()
+			{
+				if(count++ == steps)
+				{
+					oncomplete(steps, count);
+				}
+				else
+				{
+					oninstance(steps, count);
+
+					var diff = (new Date().getTime() - start) - (count * speed);
+					window.setTimeout(instance, (speed - diff));
+				}
+			}
+
+			window.setTimeout(instance, speed);
+		}
